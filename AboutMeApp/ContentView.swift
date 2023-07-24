09dd43a218b4f22d8bf63109cd9c77726b289e28
Krishi <krishi3dalal@gmail.com>
2023-07-24 13:30:35 -0400

    commit

--- AboutMeApp/ContentView.swift
+++ AboutMeApp/ContentView.swift
@@ -9,47 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var funFact = ""
+    @State private var funFact = false
     
     var body: some View {
         ZStack {
             Color(.systemPurple)
                 .ignoresSafeArea()
-            
-        VStack (alignment: .center, spacing : 15)  {
-                Text("Hello, my name is Krishi!")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.white)
-                    .multilineTextAlignment(.center)
-                    .bold()
-                Image("image")
-                    .resizable(resizingMode: .stretch)
-                    .aspectRatio(contentMode: .fit)
-                    .padding()
-                    .cornerRadius(80)
-                Text(funFact)
-                Button("Fun Fact") { funFact = "I enjoy attending wedding events with my family! I also love Shawn Mendes and the Toronto Raptors:)"
+            ScrollView {
+                VStack (alignment: .center, spacing : 15)  {
+                        Text("Hello, my name is Krishi!")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.center)
+                            .bold()
+                        Image("image")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .padding()
+                            .cornerRadius(80)
+                        Button("Fun Fact") {
+                            funFact.toggle()
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .tint(.green)
+                   
+                    Spacer()
+                
+                    if funFact {
+                        Text("I enjoy attending wedding events with my family! I also love Shawn Mendes and the Toronto Raptors:)")
+                    } else {
+                    }
+                        Image("shawn")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 200.0, height: 200.0)
+                        Image("raptors")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.horizontal)
                 }
-                .buttonStyle(.borderedProminent)
-                .tint(.green)
-           
-            Spacer()
-        
-            HStack (spacing: -25) {
-                Image("shawn")
-                    .resizable(resizingMode: .stretch)
-                    .aspectRatio(contentMode: .fit)
-                    .padding()
-                Image("raptors")
-                    .resizable(resizingMode:.stretch)
-                    .aspectRatio(contentMode: .fit)
-                    .padding()
+                .padding(.top, 20.0)
+                }
             }
-        }
-        .padding(.top, 20.0)
-        }
-    }
+            }
+      
         
     
     
